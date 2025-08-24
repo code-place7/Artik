ed05bf276a44857acfd72c7ed9094b97df48d032
MONU MANDAL <monumandel3@gmail.com>
2025-08-24 18:36:08 +0530

    added clerk middleware into backend and install inngest sdk to send webhook events from clerk to our mogodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import "dotenv/config";
 import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
+
+app.use(clerkMiddleware()); //we can check user is authenticated or not using req.auth()
 const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
