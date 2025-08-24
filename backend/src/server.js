50d309b9db4960fc57dac4bed6a6fa3ec0a01212
MONU MANDAL <monumandel3@gmail.com>
2025-08-24 19:14:02 +0530

    setup user model inngest webhook events functions and modify the server to deploy it on vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,17 +2,35 @@ import express from "express";
 import "dotenv/config";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "inngest/express";
 
 const app = express();
 
 app.use(clerkMiddleware()); //we can check user is authenticated or not using req.auth()
+app.use(express.json());
 const PORT = process.env.PORT || 5001;
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.send("Hello, World! 777");
 });
 
-app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
-  connectDB();
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (process.env.NODE_ENV !== "production") {
+      app.listen(process.env.PORT, () => {
+        console.log("Server started on port:", process.env.PORT);
+      });
+    }
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
+
+startServer();
+
+export default app;
