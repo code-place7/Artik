f75e50eba97372c0d12bc9eaefd8f2abeda85f7a
MONU MANDAL <monumandel3@gmail.com>
2025-08-29 20:20:50 +0530

    created a lib function api.js and created getStreamToken to fetch the stream token so user will get authenticate with stream to use it

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,10 @@
+import { axiosInsatnce } from "./axios";
+
+export async function getStreamToken() {
+  // Fetch the stream token from the backend
+  // This endpoint should return a token that can be used for streaming
+  //since it is get request so with axiosInstance we have to use get method and we have already set the baseURL in axiosInstance
+  // and the endpoint is /chat/token
+  const response = await axiosInsatnce.get("/chat/token");
+  return response.data;
+}
