7f81886985e97ad053143962ea34e655e76e3ce8
MONU MANDAL <monumandel3@gmail.com>
2025-08-29 19:09:59 +0530

    Added Api_base_url in environment variables

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,12 @@
 // https://artik-backend.vercel.app/
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:3000"
-    : "https://artik-backend.vercel.app/api";
+// const BASE_URL =
+//   import.meta.env.MODE === "development"
+//     ? "http://localhost:3000"
+//     : "https://artik-backend.vercel.app/api";
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
