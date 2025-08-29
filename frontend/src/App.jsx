f4c909d5be675e1d0df06e8c625ffc9aa3a72eb3
MONU MANDAL <monumandel3@gmail.com>
2025-08-29 19:29:27 +0530

    setup sentry in mainjsx and wrap the rooutes in app jsx with SentryRoutes and also made a new project on sentry named artik-frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,27 +5,30 @@ import {
   UserButton,
 } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
+import * as Sentry from "@sentry/react";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 import Hirepage from "./pages/Hirepage";
 import AboutPage from "./pages/AboutPage";
 
 export default function App() {
+  const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
+
   return (
     <>
       <SignedIn>
-        <Routes>
+        <SentryRoutes>
           <Route path="/" element={<HomePage />} />
           <Route path="/hire" element={<Hirepage />} />
           <Route path="/learn-more" element={<AboutPage />} />
           <Route path="/auth" element={<Navigate to={"/"} replace />} />
-        </Routes>
+        </SentryRoutes>
       </SignedIn>
       <SignedOut>
-        <Routes>
+        <SentryRoutes>
           <Route path="/auth" element={<AuthPage />} />
           <Route path="*" element={<Navigate to={"/auth"} replace />} />
-        </Routes>
+        </SentryRoutes>
       </SignedOut>
     </>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,21 @@
 import { StrictMode } from "react";
+import React from "react";
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
-import { BrowserRouter } from "react-router";
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router";
+import * as Sentry from "@sentry/react";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import AuthProvider from "./providers/AuthProvider.jsx";
@@ -17,6 +29,19 @@ const queryClient = new QueryClient();
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
+Sentry.init({
+  dsn: "https://81ea7ddb48553abce8a065c51777075d@o4509469392240640.ingest.us.sentry.io/4509927704231936",
+  integrations: [
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
