6ac7193ce6f99b3b2bd2397535dc05497a6f1b12
MONU MANDAL <monumandel3@gmail.com>
2025-08-29 18:51:22 +0530

    created a axiosinstance with our backend url to send tokrn with every request to backend

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,12 @@
+// https://artik-backend.vercel.app/
+import axios from "axios";
+
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : "https://artik-backend.vercel.app/api";
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true, //it means with every single request we will send token in our headers
+});
