1031860cf04aaf7328131062fdb2ab537459bd4c
MONU MANDAL <monumandel3@gmail.com>
2025-09-01 19:00:29 +0530

    creted the function for adding users to public channels in stream.ja

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,7 +1,11 @@
 import { Inngest } from "inngest";
 import { connectDB } from "../config/db.js";
 import User from "../models/user.model.js";
-import { upsertStreamUser, deleteStreamUser } from "./stream.js";
+import {
+  upsertStreamUser,
+  deleteStreamUser,
+  addUserToPublicChannels,
+} from "./stream.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "artik" });
